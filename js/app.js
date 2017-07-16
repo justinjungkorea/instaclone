7bff77eace446b82d337599c959f5368f848c790
Justin_Jung <jungdw0624@gmail.com>
2017-07-16 17:40:12 +0900

    Final maybee...?

--- js/app.js
+++ js/app.js
@@ -24,15 +24,80 @@
 
 $(document).ready(function() {
   var $heart = $(".heart");
+
   $heart.click(function() {
+    var $likes = $(".photo__likes");
+    var likesnumber = parseInt($likes.html());
+
+    if ($(this).hasClass("fa-heart-o")) {
+      ++likesnumber;
+    } else {
+      --likesnumber;
+    }
+    $likes.html(likesnumber);
+
     $(this).toggleClass("fa-heart-o");
     $(this).toggleClass("fa-heart heart-click");
   })
 
   var $comment = $(".comment");
+
   $comment.click(function() {
+    var $comment_number = $(".photo__comment--number");
+    var commentNumber = parseInt($comment_number.html());
+
+    if ($(this).hasClass("fa-comment-o")) {
+      ++commentNumber;
+    } else {
+
+    }
+    $comment_number.html(commentNumber);
+
     $(this).toggleClass("fa-comment-o");
     $(this).toggleClass("fa-comment comment-click");
   })
 
-})
+  var $button = $(".baaam");
+  var $feedbutton = $(".bam");
+  var $overlay = $(".profileOverlay");
+  var $feedoverlay = $(".FeedOverlay");
+
+  $button.click(function() {
+    $overlay.fadeIn(500);
+  });
+
+  $feedbutton.click(function() {
+    $feedoverlay.fadeIn(500);
+  });
+
+
+
+
+  var $cancel = $(".cancel__button");
+  var $newcancel = $(".profileOverlay i");
+  var $newfeedcancel = $(".FeedOverlay i");
+  var $feedcancel = $(".feed__cancel");
+
+  $cancel.click(function() {
+    $overlay.fadeOut(500);
+  });
+
+  $newcancel.click(function() {
+    $overlay.fadeOut(500);
+  });
+
+  $feedcancel.click(function() {
+    $feedoverlay.fadeOut(500);
+  });
+
+  $newfeedcancel.click(function() {
+    $feedoverlay.fadeOut(500);
+  });
+
+
+
+
+
+
+
+});
