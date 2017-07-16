423fa35f39724957165145cf9c1c4313d9cdf2c3
Justin_Jung <jungdw0624@gmail.com>
2017-07-16 15:53:00 +0900

    click event

--- js/app.js
+++ js/app.js
@@ -1,25 +1,38 @@
-var weaponsList = ["AK-47", "Gun", "Machete", "Knife"];
-var countries = ["Korea", "Japan", "Greece", "Colombia"];
+// var weaponsList = ["AK-47", "Gun", "Machete", "Knife"];
+// var countries = ["Korea", "Japan", "Greece", "Colombia"];
+//
+// var soldier = {
+//   name: "Nicolas",
+//   weapon: weaponsList[2],
+//   country: countries[3],
+//   isDead: false,
+//   shoot: function() {
+//     console.log("Pew Pew");
+//   },
+//   Hello: function(person) {
+//     console.log("Hello" + person + " My name is " + this.name);
+//   }
+// };
+//
+// function multiply(one, two) {
+//   return one * two;
+// }
+//
+// function sum(one, two) {
+//   return one + two;
+// }
 
-var soldier = {
-  name: "Nicolas",
-  weapon: weaponsList[2],
-  country: countries[3],
-  isDead: false,
-  shoot: function() {
-    console.log("Pew Pew");
-  },
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function() {
+    $(this).toggleClass("fa-heart-o");
+    $(this).toggleClass("fa-heart heart-click");
+  })
 
-  Hello: function(person) {
-    console.log("Hello" + person + " My name is " + this.name);
-  }
-};
+  var $comment = $(".comment");
+  $comment.click(function() {
+    $(this).toggleClass("fa-comment-o");
+    $(this).toggleClass("fa-comment comment-click");
+  })
 
-
-function multiply(one, two) {
-  return one * two;
-}
-
-function sum(one, two) {
-  return one + two;
-}
+})
